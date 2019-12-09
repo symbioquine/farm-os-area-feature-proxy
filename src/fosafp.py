4afb31275bb1af1430ae0ef5354486a4aa36b624
Symbioquine <symbioquine@gmail.com>
2019-12-09 00:01:00 -0800

    Working point, polygon, and point string feature display

--- src/fosafp.py
+++ src/fosafp.py
@@ -8,7 +8,7 @@ from twisted.web.static import File
 from twisted.web.resource import Resource
 from twisted.internet import reactor
 
-from lxml import etree
+from lxml import etree, objectify
 from lxml.builder import E, ElementMaker  # lxml only !
 
 WFS_PROTOCOL_VERSION = "1.1.0"
@@ -27,18 +27,9 @@ NAMESPACES = {
     'xsi': "http://www.w3.org/2001/XMLSchema-instance",
 }
 
-import farmOS
+import farmOS, json, re
 
-farm = farmOS.farmOS('http://172.17.0.2', 'FarmOS.restws.zero', 'zsARb1hZjFwK0jMIh3Td')
-success = farm.authenticate()
-
-info = farm.info()
-
-print(info)
-
-areas = farm.session.http_request("farm/areas/geojson")
-
-print(areas.json())
+from itertools import chain
 
 class DictAccessor(object):
     def __init__(self, d):
@@ -72,6 +63,138 @@ gml = nsE.gml
 ms = nsE.ms
 
 
+AREA_DESCRIPTION_ID_PATTERN = re.compile('area-details-(\\d+)')
+
+class PointFeatureMemberFactory(object):
+    relevant_type_name = "Point"
+
+    def to_feature_member(self, type_name, feature):
+        geometry = feature.get('geometry', {})
+        properties = feature.get('properties', {})
+
+        area_name = etree.fromstring(properties.get('name', '')).text
+        area_id_match = AREA_DESCRIPTION_ID_PATTERN.match(objectify.fromstring(properties.get('description', '')).get('id'))
+
+        if not area_id_match:
+            return None
+
+        area_id = area_id_match.group(1)
+
+        coordinates = geometry.get('coordinates', None)
+
+        return gml.featureMember(
+            ms.farm_os_features_point(
+                ms.geometry(
+                    gml.Point(
+                        gml.pos(
+                            "{} {}".format(coordinates[0], coordinates[1])
+                        ),
+                        srsName=WFS_PROJECTION
+                    )
+                ),
+                ms.area_name(area_name),
+                nsAttr.gml.id("{type_name}.{area_id}".format(type_name=type_name, area_id=area_id))
+            )
+        )
+
+class PolygonFeatureMemberFactory(object):
+    relevant_type_name = "Polygon"
+
+    def to_feature_member(self, type_name, feature):
+        geometry = feature.get('geometry', {})
+        properties = feature.get('properties', {})
+
+        area_name = etree.fromstring(properties.get('name', '')).text
+        area_id_match = AREA_DESCRIPTION_ID_PATTERN.match(objectify.fromstring(properties.get('description', '')).get('id'))
+
+        if not area_id_match:
+            return None
+
+        area_id = area_id_match.group(1)
+
+        coordinates = geometry.get('coordinates', [])
+
+        pos_list = " ".join(map(str, (chain.from_iterable(chain.from_iterable(coordinates)))))
+
+        return gml.featureMember(
+            ms.farm_os_features_polygon(
+                ms.geometry(
+                    gml.Polygon(
+                        gml.exterior(
+                            gml.LinearRing(
+                                gml.posList(
+                                    pos_list,
+                                    srsDimension="2"
+                                )
+                            )
+                        ),
+                        srsName=WFS_PROJECTION
+                    )
+                ),
+                ms.site_name(area_name),
+                nsAttr.gml.id("{type_name}.{area_id}".format(type_name=type_name, area_id=area_id))
+            )
+        )
+
+class PointStringFeatureMemberFactory(object):
+    relevant_type_name = "LineString"
+
+    def to_feature_member(self, type_name, feature):
+        geometry = feature.get('geometry', {})
+        properties = feature.get('properties', {})
+
+        area_name = etree.fromstring(properties.get('name', '')).text
+        area_id_match = AREA_DESCRIPTION_ID_PATTERN.match(objectify.fromstring(properties.get('description', '')).get('id'))
+
+        if not area_id_match:
+            return None
+
+        area_id = area_id_match.group(1)
+
+        coordinates = geometry.get('coordinates', [])
+
+        pos_list = " ".join(map(str, (chain.from_iterable(coordinates))))
+
+        return gml.featureMember(
+            ms.farm_os_features_line_string(
+                ms.geometry(
+                    gml.LineString(
+                        gml.posList(
+                            pos_list,
+                            srsDimension="2"
+                        ),
+                        srsName=WFS_PROJECTION
+                    )
+                ),
+                ms.site_name(area_name),
+                nsAttr.gml.id("{type_name}.{area_id}".format(type_name=type_name, area_id=area_id))
+            )
+        )
+
+FEATURE_MEMBER_FACTORIES = {
+    'farm_os_features_point': PointFeatureMemberFactory(),
+    'farm_os_features_polygon': PolygonFeatureMemberFactory(),
+    'farm_os_features_line_string': PointStringFeatureMemberFactory()
+}
+
+def relevant(factory, feature):
+    geometry = feature.get('geometry', {})
+    geometry_type = geometry.get('type', None)
+
+    return geometry_type == factory.relevant_type_name
+
+def to_type_filtered_feature_members(type_name, features):
+    factory = FEATURE_MEMBER_FACTORIES.get(type_name, None)
+
+    if not factory:
+        return ()
+
+    relevant_features = filter(partial(relevant, factory), features)
+
+    feature_members = map(partial(factory.to_feature_member, type_name), relevant_features)
+
+    return filter(None, feature_members)
+
 class FarmOsAreaFeatureProxy(Resource):
     isLeaf = True
 
@@ -83,15 +206,15 @@ class FarmOsAreaFeatureProxy(Resource):
 
         print(args)
 
-        request_type = args.get(b'request')
+        request_type = args.get(b'request')[0]
 
         doc = None
-        if request_type == [b'GetCapabilities']:
+        if request_type == b'GetCapabilities':
             doc = self._get_capabilities()
-        elif request_type == [b'DescribeFeatureType']:
-            doc = self._describe_feature_type(request)
-        elif request_type == [b'GetFeature']:
-            doc = self._get_feature(request)
+        elif request_type == b'DescribeFeatureType':
+            doc = self._describe_feature_type(args)
+        elif request_type == b'GetFeature':
+            doc = self._get_feature(request, args)
 
         if not doc is None:
             request.setHeader('Content-Type', 'text/xml')
@@ -106,14 +229,24 @@ class FarmOsAreaFeatureProxy(Resource):
 
         print(request.content.read())
 
-    def _get_feature(self, request):
+    def _get_feature(self, request, args):
+        type_name = args.get(b'typename')[0].decode('utf-8')
+
+        farm = farmOS.farmOS('http://172.17.0.2', request.getUser(), request.getPassword())
+        farm.authenticate()
+
+        areas = farm.session.http_request("farm/areas/geojson").json()
+
+        type_filtered_feature_members = to_type_filtered_feature_members(type_name, areas.get('features', []))
+
         return wfs.FeatureCollection(
             nsAttr.xsi.schemaLocation(("http://mapserver.gis.umn.edu/mapserver "
-                                      + "http://localhost:5707?SERVICE=WFS&VERSION={WFS_PROTOCOL_VERSION}&REQUEST=DescribeFeatureType&TYPENAME=antarctic_ice_shelves_fill&OUTPUTFORMAT={WFS_MIMETYPE}; "
+                                      + "http://localhost:5707?SERVICE=WFS&VERSION={WFS_PROTOCOL_VERSION}&REQUEST=DescribeFeatureType&TYPENAME={type_name}&OUTPUTFORMAT={WFS_MIMETYPE}; "
                                       + "subtype={GML_VERSION} http://www.opengis.net/wfs http://schemas.opengis.net/wfs/{WFS_PROTOCOL_VERSION}/wfs.xsd").format(
                                           WFS_PROTOCOL_VERSION=WFS_PROTOCOL_VERSION,
                                           WFS_MIMETYPE=WFS_MIMETYPE,
-                                          GML_VERSION=GML_VERSION)),
+                                          GML_VERSION=GML_VERSION,
+                                          type_name=type_name)),
             gml.boundedBy(
                 gml.Envelope(
                     gml.lowerCorner("-122.9273871146143 48.6652415209444"),
@@ -121,68 +254,20 @@ class FarmOsAreaFeatureProxy(Resource):
                     srsName=WFS_PROJECTION
                 )
             ),
-            #gml.featureMember(
-            #    ms.antarctic_ice_shelves_fill(
-            #        ms.geometry(
-            #            gml.Point(
-            #                gml.pos(
-            #                    "-122.9273381642997 48.6652266847594"
-            #                ),
-            #                srsName=WFS_PROJECTION
-            #            )
-            #        ),
-            #        ms.site_name("Coolidge6"),
-            #        nsAttr.gml.id("antarctic_ice_shelves_fill.6")
-            #    )
-            #),
-            #gml.featureMember(
-            #    ms.antarctic_ice_shelves_fill(
-            #        ms.geometry(
-            #            gml.Point(
-            #                gml.pos(
-            #                    "-122.9273381642997 48.6652266847594"
-            #                ),
-            #                srsName=WFS_PROJECTION
-            #            )
-            #        ),
-            #        ms.site_name("Coolidge7"),
-            #        nsAttr.gml.id("antarctic_ice_shelves_fill.7")
-            #    )
-            #),
-            gml.featureMember(
-                ms.antarctic_ice_shelves_fill(
-                    ms.geometry(
-                        gml.Polygon(
-                            gml.exterior(
-                                gml.LinearRing(
-                                    gml.posList(
-                                         "-122.9273381642997 48.6652266847594 "
-                                        +"-122.9273411817849 48.66524152094448 "
-                                        +"-122.9273871146143 48.66523753510415 "
-                                        +"-122.9273844324052 48.66522269891789 "
-                                        +"-122.9273381642997 48.6652266847594",
-                                        srsDimension="2"
-                                    )
-                                )
-                            ),
-                            srsName=WFS_PROJECTION
-                        )
-                    ),
-                    ms.site_name("Coolidge8"),
-                    nsAttr.gml.id("antarctic_ice_shelves_fill.8")
-                )
-            )
+            *type_filtered_feature_members
         )
 
-    def _describe_feature_type(self, request):
+    def _describe_feature_type(self, args):
+        type_name = args.get(b'typename')[0].decode('utf-8')
+
         return bare.schema(
             E("import",
                 namespace=ns.gml,
                 schemaLocation="http://schemas.opengis.net/{GML_VERSION}/base/gml.xsd".format(GML_VERSION=GML_VERSION)
             ),
             E.element(
-                name="antarctic_ice_shelves_fill",
-                type="ms:antarctic_ice_shelves_fillType",
+                name=type_name,
+                type="ms:{}Type".format(type_name),
                 substitutionGroup="gml:_Feature"
             ),
             E.complexType(
@@ -190,12 +275,12 @@ class FarmOsAreaFeatureProxy(Resource):
                     E.extension(
                         E.sequence(
                             E.element(name="geometry", type="gml:GeometryPropertyType", minOccurs="0", maxOccurs="1"),
-                            E.element(name="site_name", type="string")
+                            E.element(name="area_name", type="string")
                         ),
                         base="gml:AbstractFeatureType"
                     )
                 ),
-                name="antarctic_ice_shelves_fillType"
+                name="{}Type".format(type_name)
             ),
             xmlns=ns.xsd,
             targetNamespace=ns.ms,
@@ -225,12 +310,34 @@ class FarmOsAreaFeatureProxy(Resource):
             E.FeatureTypeList(
                 E.Operations(E.Query, E.Insert, E.Unsert, E.Delete, E.Lock),
                 E.FeatureType(
-                    E.Name("antarctic_ice_shelves_fill"),
-                    E.Title("Antarctic ice shelves"),
-                    ows.Keywords(
-                        ows.Keyword("Antarctica"),
-                        ows.Keyword("Coastlines")
+                    E.Name("farm_os_features_point"),
+                    E.Title("FarmOS point features"),
+                    E.DefaultSRS(WFS_PROJECTION),
+                    E.OutputFormats(
+                        E.Format("{WFS_MIMETYPE}; subtype={GML_VERSION}".format(WFS_MIMETYPE=WFS_MIMETYPE, GML_VERSION=GML_VERSION))
                     ),
+                    ows.WGS84BoundingBox(
+                        ows.LowerCorner("-122.9273871146143 48.6652415209444"),
+                        ows.UpperCorner("-122.9273381642997 48.6652226989178"),
+                        dimensions="2"
+                    )
+                ),
+                E.FeatureType(
+                    E.Name("farm_os_features_polygon"),
+                    E.Title("FarmOS polygon features"),
+                    E.DefaultSRS(WFS_PROJECTION),
+                    E.OutputFormats(
+                        E.Format("{WFS_MIMETYPE}; subtype={GML_VERSION}".format(WFS_MIMETYPE=WFS_MIMETYPE, GML_VERSION=GML_VERSION))
+                    ),
+                    ows.WGS84BoundingBox(
+                        ows.LowerCorner("-122.9273871146143 48.6652415209444"),
+                        ows.UpperCorner("-122.9273381642997 48.6652226989178"),
+                        dimensions="2"
+                    )
+                ),
+                E.FeatureType(
+                    E.Name("farm_os_features_line_string"),
+                    E.Title("FarmOS line string features"),
                     E.DefaultSRS(WFS_PROJECTION),
                     E.OutputFormats(
                         E.Format("{WFS_MIMETYPE}; subtype={GML_VERSION}".format(WFS_MIMETYPE=WFS_MIMETYPE, GML_VERSION=GML_VERSION))
